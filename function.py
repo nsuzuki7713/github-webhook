1e22563cf673b975fec22814613ad0546adfe709
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-04-06 22:42:57 +0900

    fix-apigateway-error

--- function.py
+++ function.py
@@ -1,17 +1,14 @@
 #coding: UTF-8
 from requests_oauthlib import OAuth1Session
-# import json
+from datetime import datetime
 import settings
 
 def lambda_handler(event, context):
     twitter = OAuth1Session(settings.CONSUMER_KEY, settings.CONSUMER_SECRET, settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
 
-    # params = {"status": "ローカルのpythonからつぶやいているぞー。呟けている？？ #shinjukumokumoku"}
-    params = {"status": "AWSのLambdaからつぶやいているぞー。テスト6"}
+    params = {"status": "テストツイートしてるぞー(" + datetime.now().strftime("%H:%M:%S") + ")"}
     req = twitter.post("https://api.twitter.com/1.1/statuses/update.json",params = params)
-
+    
     print('つぶやけたぞー')
     
-    return { 
-        'message' : "つぶやけたぞー"
-    }
+    return {"statusCode": 200, "body": "つぶやけたぞー" }
