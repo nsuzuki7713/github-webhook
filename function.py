73cbd0ef39117f40217080a9641fecb47cd5f5f6
nsuzuki7713 <nsuzuki7713@gmail.com>
2019-04-07 15:53:03 +0900

    add-certification

--- function.py
+++ function.py
@@ -1,14 +1,21 @@
 #coding: UTF-8
+import json
+import hashlib
+import hmac
 from requests_oauthlib import OAuth1Session
 from datetime import datetime
 import settings
 
 def lambda_handler(event, context):
-    twitter = OAuth1Session(settings.CONSUMER_KEY, settings.CONSUMER_SECRET, settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
+    # HMAC値による簡易認証処理
+    signature = event['headers']['X-Hub-Signature']
+    signedBody = "sha1=" + hmac.new(bytes(settings.SECRET, 'ascii'), bytes(event['body'], 'ascii'), hashlib.sha1).hexdigest()
+    if(signature != signedBody):
+        return {"statusCode": 401, "body": "Unauthorized" }
 
+    twitter = OAuth1Session(settings.CONSUMER_KEY, settings.CONSUMER_SECRET, settings.ACCESS_TOKEN, settings.ACCESS_TOKEN_SECRET)
     params = {"status": "テストツイートしてるぞー(" + datetime.now().strftime("%H:%M:%S") + ")"}
     req = twitter.post("https://api.twitter.com/1.1/statuses/update.json",params = params)
     
-    print('つぶやけたぞー')
-    
     return {"statusCode": 200, "body": "つぶやけたぞー" }
+
